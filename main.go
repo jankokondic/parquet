0a69fd2c343fe8c894ec7db7a7d07109d20a2e30
WatchJani <jankokondic84@gmail.com>
2025-11-21 14:32:27 +0100

    rowgroup and page header

--- main.go
+++ main.go
@@ -10,13 +10,7 @@ import (
 	"github.com/apache/thrift/lib/go/thrift"
 )
 
-func ReadParquetFooter(path string) (*parquet.FileMetaData, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, fmt.Errorf("failed to open parquet file: %w", err)
-	}
-	defer f.Close()
-
+func ReadParquetFooter(f *os.File) (*parquet.FileMetaData, error) {
 	// Stat to get size
 	stat, err := f.Stat()
 	if err != nil {
@@ -66,6 +60,54 @@ func ReadParquetFooter(path string) (*parquet.FileMetaData, error) {
 	return meta, nil
 }
 
+func ReadPageHeader(f *os.File, offset int64) (*parquet.PageHeader, error) {
+	// Seek to the beginning of the page header
+	_, err := f.Seek(offset, 0)
+	if err != nil {
+		return nil, fmt.Errorf("seek failed: %w", err)
+	}
+
+	// Create a reader that wraps the file
+	transport := thrift.NewStreamTransportR(f)
+	protocol := thrift.NewTCompactProtocolFactoryConf(&thrift.TConfiguration{}).GetProtocol(transport)
+
+	header := parquet.NewPageHeader()
+	err = header.Read(context.Background(), protocol)
+	if err != nil {
+		return nil, fmt.Errorf("failed reading page header: %w", err)
+	}
+
+	return header, nil
+}
+
 func main() {
-	ReadParquetFooter("./flights-1m.parquet")
+	f, err := os.Open("./flights-1m.parquet")
+	if err != nil {
+		fmt.Printf("failed to open parquet file: %e", err)
+	}
+	defer f.Close()
+
+	meta, err := ReadParquetFooter(f)
+	if err != nil {
+		fmt.Printf("failed to read parquet file: %e", err)
+	}
+
+	//stats
+	for i, rg := range meta.RowGroups {
+		for j, col := range rg.Columns {
+			dpo := col.MetaData.DataPageOffset
+			fmt.Printf("\nRowGroup %d, Column %d:\n", i, j)
+			fmt.Println("  DataPageOffset:", dpo)
+
+			header, err := ReadPageHeader(f, dpo)
+			if err != nil {
+				fmt.Printf("  FAILED reading page header: %v\n", err)
+				continue
+			}
+
+			fmt.Printf("  Page type: %v\n", header.Type)
+			// fmt.Printf("  Uncompressed size: %v\n", header.Uncompressed_page_size)
+			// fmt.Printf("  Compressed size: %v\n", header.Compressed_page_size)
+		}
+	}
 }
